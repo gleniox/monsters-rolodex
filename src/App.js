ec00364b0db37b8808702105b8a5ebe95b3d98b3
Glenio Alexandre Nogueira <gleniox@outlook.com>
2022-06-27 16:58:20 -0300

    Add component state

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Glenio",
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello world!</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Hi {this.state.name}</p>
+          <button>Change Name</button>
         </header>
       </div>
     );
