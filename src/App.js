5c54a5408b3c8cf3bdc7adbdca1c9f3851e29e8a
Glenio Alexandre Nogueira <gleniox@outlook.com>
2022-06-30 16:01:10 -0300

    Search monsters

--- src/App.js
+++ src/App.js
@@ -42,6 +42,12 @@ class App extends Component {
           placeholder="Search monsters"
           onChange={(event) => {
             console.log(event.target.value);
+
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.includes(event.target.value);
+            });
+
+            this.setState({ monsters: filteredMonsters });
           }}
         />
         {this.state.monsters.map((monster) => {
