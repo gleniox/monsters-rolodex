ba2a79aef9852026d39e4fd03729b28cf8705965
Glenio Alexandre Nogueira <gleniox@outlook.com>
2022-08-05 09:39:21 -0300

    Add css

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -5,14 +5,21 @@ import { PropTypes } from "prop-types";
 
 class CardList extends Component {
   render() {
-    console.log('render from card-list');
+    console.log("render from card-list");
     const { monsters } = this.props;
     console.log(this.props);
 
     return (
       <div>
         {monsters.map((monster) => {
-          return <h1 key={monster.id}>{monster.name}</h1>;
+          return <div className="card-container" key={monster.id}>
+            <img
+              alt={`monster ${monster.name}`}
+              src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+            />
+            <h2>{monster.name}</h2>
+            <p>{monster.email}</p>
+          </div>;
         })}
       </div>
     );
