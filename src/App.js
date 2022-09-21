c52f129478779a65520a095b387857895b1d457b
Glenio Alexandre Nogueira <gleniox@outlook.com>
2022-09-21 16:44:52 -0300

    Added useuseEffect hook

--- src/App.js
+++ src/App.js
@@ -3,20 +3,33 @@ import React, { useState } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
+import { useEffect } from "react";
 
 const App = () => {
   console.log("render");
   const [monsterSearch, setMonsterSearch] = useState("");
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    console.log("monsters effect fired");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    console.log("filteredMonsters effect fired");
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(monsterSearch);
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, monsterSearch]);
 
   const onMonsterSearchChange = (event) => {
     const monsterSearchString = event.target.value.toLowerCase();
     setMonsterSearch(monsterSearchString);
   };
-
-  const filteredMonsters = monsters.filter((monster) => {
-    return monster.name.toLowerCase().includes(monsterSearch);
-  });
 
   return (
     <div className="App">
